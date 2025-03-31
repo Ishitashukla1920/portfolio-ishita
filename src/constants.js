f19a0e15b6adda05db3affe70fa1b4e7092000b3
Ishita Shukla <ishita20jan@gmail.com>
2025-03-31 12:53:22 +0530

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,24 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
+    I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/Ishitashukla1920" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and unwind with some interesting content.  
+  I also enjoy exploring creative projects and sharing my work with others.  
+  This portfolio is one of those projects I'm excited about, and I hope you enjoy it as much as I enjoyed creating it!`,
+
+tv: `That's my TV. I've been exploring various tech and programming resources recently to keep learning and growing.  
+  Watching experts and creators online is always inspiring and helps me stay motivated.`,
+
+bed: `This is where I sleep. Some of my best ideas come when I'm lying on my bed.  
+  When inspiration strikes, I make sure to jot it down quickly, or it keeps me up all night!`,
+resume:`This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1CRtydIyE1NWrItQMIjW4jVq_RPbfp1Dp/view?usp=sharing" target="_blank">Check it out?</a>
+  Contact me at ishita20jan@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `There are a lot of programming books on my shelves.
+  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit Ishita's portfolio, just close the tab.`,
+};
